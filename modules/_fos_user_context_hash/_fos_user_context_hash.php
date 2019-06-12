e4e77c6aee49bc34c5b752fa47599d980ffa2cfb
Luca Realdi <lr@opencontent.it>
2019-06-12 17:06:13 +0200

    _fos_user_context_hash redirige a indexpage se accetp non è vnd.fos.user-context-hash

--- modules/_fos_user_context_hash/_fos_user_context_hash.php
+++ modules/_fos_user_context_hash/_fos_user_context_hash.php
@@ -1,14 +1,16 @@
 <?php
 
-$hash = Opencontent\FosHttpCache\HashGenerator::instance()->generateHash();
+/** @var eZModule $module */
+$module = $Params['Module'];
 
 if ('application/vnd.fos.user-context-hash' == strtolower($_SERVER['HTTP_ACCEPT'])) {
+    $hash = Opencontent\FosHttpCache\HashGenerator::instance()->generateHash();
     header(sprintf('X-User-Context-Hash: %s', $hash));
     header('Content-Type: application/vnd.fos.user-context-hash');
     header('Cache-Control: max-age=3600');
-    header('Vary: cookie, authorization');    
+    header('Vary: cookie, authorization');
     eZExecution::cleanExit();
 }
 
-header('HTTP/1.1 406');
-eZExecution::cleanExit();
+$module->redirectTo('/');
+return;
