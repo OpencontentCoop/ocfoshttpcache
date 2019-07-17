aa42fb96d3dc6e01c68810e1623aab43dde34551
Luca Realdi <lr@opencontent.it>
2019-07-17 11:29:55 +0200

    non invia header cache per _fos_user_context_hash

--- modules/_fos_user_context_hash/_fos_user_context_hash.php
+++ modules/_fos_user_context_hash/_fos_user_context_hash.php
@@ -7,8 +7,8 @@ if ('application/vnd.fos.user-context-hash' == strtolower($_SERVER['HTTP_ACCEPT'
     $hash = Opencontent\FosHttpCache\HashGenerator::instance()->generateHash();
     header(sprintf('X-User-Context-Hash: %s', $hash));
     header('Content-Type: application/vnd.fos.user-context-hash');
-    header('Cache-Control: max-age=3600');
-    header('Vary: cookie, authorization');
+    //header('Cache-Control: max-age=3600');
+    //header('Vary: cookie, authorization');
     eZExecution::cleanExit();
 }
 
